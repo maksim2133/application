56e3324ad9a75959801f9eda2a93b1e518a0391b
flexmo0n <gushhin-02@mail.ru>
2022-05-04 17:45:01 +0300

    Add files via upload

---
+++ application/application/SigninViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  application
+//
+//  Created by Student on 04.05.2022.
+//
+
+import UIKit
+
+class SigninViewController: UIViewController {
+
+    override func viewDidLoad() {
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
